46b1ccc10a5dda2c15ebb2b8d5d625f57cda8f08
Uzodinma Ufondu <uzodinmau@uzodinmas-macbook-pro.local>
2020-05-11 14:04:32 -0500

    added cloud connection

--- mongo.js
+++ mongo.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/synapse')
+
+const uri = "mongodb+srv://uzoufondu:&123Canon@mycluster-se2sm.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('connected to MongoDB database'))
         .catch(err => console.error('could not connect to MongoDB', err))
 
