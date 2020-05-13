438f47727552295172a0f86088ab491a573e3d53
ufondu88 <41022629+ufondu88@users.noreply.github.com>
2020-05-13 16:20:59 -0500

    fixed posts

--- models/post.js
+++ models/post.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 const User = require('./user');
 
+// const uri = "mongodb+srv://uzoufondu:&123Canon@mycluster-se2sm.mongodb.net/test?retryWrites=true&w=majority";
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//         .then(() => console.log('connected to MongoDB database'))
+//         .catch(err => console.error('could not connect to MongoDB', err))
+
 const Post = mongoose.model('Posts', new mongoose.Schema({
     author: { 
         type: mongoose.Schema.Types.ObjectId, 
@@ -20,10 +26,23 @@ function validatePost(post){
         likes: Joi.array().items(Joi.string()),
         comments: Joi.array().items(Joi.string()),
         content: Joi.string().required(),
-        date: Joi.date().required()
+        
     }
     return Joi.validate(post, schema);
 }
 
+// async function createPost(){
+//     const post = new Post({
+//         author: "5ebb2ea9163d3972c35ebf75",
+//         content: "Wooo! MEAN!!",
+//     })
+
+//     const result = await post.save();
+//     console.log(result);
+    
+// }
+
+// createPost();
+ 
 exports.Post = Post;
 exports.validate = validatePost
