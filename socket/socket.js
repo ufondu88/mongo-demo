105224c882db0d700a2d04e3a46a44dc76df785e
ufondu88 <41022629+ufondu88@users.noreply.github.com>
2020-05-28 00:25:08 -0500

    started adding to socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,23 +1,54 @@
 const { Chatroom } = require('../models/chatroom');
+const { Post } = require('../models/post');
 const http = require('http').createServer();
 const io = require('../node_modules/socket.io')(http);
 
 let rooms = []
+let posts = []
+
 http.listen(4000, () => console.log(`socket listening on port 4000...`))
 
-async function chatrooms() {
-    return await Chatroom.find().then(chatrooms => {
-        chatrooms.forEach(chatroom => rooms.push(JSON.stringify(chatroom._id)));
-        startSocket()
+function start(){
+    getChatrooms()
+    getPosts()
+
+    openSocket()
+}
+
+async function getChatrooms() {
+    return await Chatroom.find().then(res => {
+        res.forEach(chatroom => rooms.push(JSON.stringify(chatroom._id)));
     })
 }
 
-function startSocket(){
+async function getPosts() {
+    return await Post.find().then(res => {
+        res.forEach(post => posts.push(JSON.stringify(post._id)));
+    })
+}
+
+function openSocket(){
+    openChatroomConnection()
+}
+
+function openChatroomConnection(){
     io
     .of('/messages')
     .on('connection', (socket) => {
-        socket.emit('welcome', 'Hello and welcome to the chat area')
+        socket.on('joinRoom', (data) => {
+            if (rooms.includes(JSON.stringify(data.message.chatroom))) {
+                socket.emit('newMessage', data)
+            } else {
+                return socket.emit('error', `${data.message.chatroom} does not exist`)
+            }
+        })
+    })
+}
 
+function openProfileConnection(){
+    io
+    .of('/profiles')
+    .on('connection', (socket) => {
         socket.on('joinRoom', (data) => {
             if (rooms.includes(JSON.stringify(data.message.chatroom))) {
                 socket.emit('newMessage', data)
@@ -29,7 +60,8 @@ function startSocket(){
 }
 
 
-chatrooms()
+
+start()
 
 exports.io = io;
 exports.http = http;
