b08196ee1aa1e2af0c0fafbe14c42ec3fc3707a3
Uzodinma Ufondu <uzodinmau@Uzodinmas-MacBook-Pro.local>
2020-05-27 22:46:49 -0500

    finished socket

--- socket/socket.js
+++ socket/socket.js
@@ -2,27 +2,34 @@ const { Chatroom } = require('../models/chatroom');
 const http = require('http').createServer();
 const io = require('../node_modules/socket.io')(http);
 
-async function chatrooms(){
-    return await Chatroom.find().then(res => console.log(res))
+let rooms = []
+http.listen(4000, () => console.log(`socket listening on port 4000...`))
+
+async function chatrooms() {
+    return await Chatroom.find().then(chatrooms => {
+        chatrooms.forEach(chatroom => rooms.push(JSON.stringify(chatroom._id)));
+        startSocket()
+    })
 }
 
-chatrooms()
-const rooms = ['red', 'blue', 'yellow']
-io.of('/messages')
+function startSocket(){
+    io
+    .of('/messages')
     .on('connection', (socket) => {
         socket.emit('welcome', 'Hello and welcome to the chat area')
 
-        socket.on('joinRoom', (room) => {
-            if (rooms.includes(room)){
-                socket.join(room)
-                return socket.emit('success', `You have suiccessfully joined the ${room} room`)
+        socket.on('joinRoom', (data) => {
+            if (rooms.includes(JSON.stringify(data.message.chatroom))) {
+                socket.emit('newMessage', data)
             } else {
-                return socket.emit('error', `${room} does not exist`)
+                return socket.emit('error', `${data.message.chatroom} does not exist`)
             }
         })
     })
+}
 
-http.listen(4000, () => console.log(`socket listening on port 4000...`))
+
+chatrooms()
 
 exports.io = io;
 exports.http = http;
