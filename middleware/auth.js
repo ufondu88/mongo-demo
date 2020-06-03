e9b221e3fa7812a728ea23e48574560ab8c9e0e7
ufondu88 <41022629+ufondu88@users.noreply.github.com>
2020-06-03 02:05:37 -0500

    log

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next){
-    console.log('in here')
     const token = req.query.token;
     if (!token) return res.status(401).send('Access Denied. No token provided');
 
