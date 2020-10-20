655d9e212d41b2f91f2dbad2ce27990b949de062
ufondu88 <uzoufondu@gmail.com>
2020-10-19 20:06:10 -0500

    change

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  console.log(query)
+  console.log(req.query)
   const token = req.query.synapse;
   if (!token) return res.status(401).send("Access Denied. No token provided");
 
